b5e6edbe86b3d8ce4320ca2961f544e8cf19b370
KillyBOT <edwards.kyle.j@gmail.com>
2019-09-06 10:53:48 -0400

    A bit more work, currently on the registers.

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Structure for registers
+
+#define ZERO_PAGE_START 0x0000
+#define ZERO_PAGE_END 0x00ff
+
+#define INTERRUPER_HANDLER 0xfffa
+#define POWER_RESET 0xfffc
+#define INTERRUPT_REQUEST_HANDLER 0xfffe
+
+typedef short address;
+typedef char reg;
+
+typedef struct {
+  
+} registers;
+
 int main(){
-  printf("Finished\n", );
+  printf("Finished\n");
   return 0;
 }
