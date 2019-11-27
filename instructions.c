00ade2f31e50607c7f84d6b363990303af78df60
KillyBOT <edwards.kyle.j@gmail.com>
2019-11-27 10:59:17 -0500

    Adding non-specific operations

--- instructions.c
+++ instructions.c
@@ -52,7 +52,7 @@ void printProcessor(struct processor* p){
 
 void printStack(struct processor* p){
   if(p->stPtr == 0) printf("Empty stack\n");
-  else memDump(p->mem, SYSTEM_STACK_BEGIN, p->stPtr);
+  else memDump(p->mem, SYSTEM_STACK_BEGIN, (p->stPtr / 8) + 1);
 }
 
 int doCycle(struct processor* p){
@@ -65,7 +65,7 @@ int doCycle(struct processor* p){
 
   //Check flags
 
-  if((p->status & 4) == 4){
+  if((p->status & B) == B){
     printf("Program interrupted\n");
     return 0;
   }
@@ -73,39 +73,42 @@ int doCycle(struct processor* p){
   reg_8 currentInstruction;
   reg_8 read_8; // Zero page and immediate
   reg_16 read_16;
+  reg_16 newPCount;
   char pToAdd;
   reg_16 sumTest;
-  long cyclesToAdd;
+  long cToAdd;
 
   currentInstruction = *(p->mem + p->pCount);
   read_8 = *(p->mem + p->pCount + 1);
   read_16 = getFlipped(p->mem + p->pCount, 1, 2);
   pToAdd = 2;
-  cyclesToAdd = 2;
+  cToAdd = 2;
   /*printf("1 byte read: %.2X\n", read_8);
   printf("2 byte read: %.4X\n", read_16);*/
 
   printf("Current Instruction: ");
 
   switch(currentInstruction){
+    //Specific operations
+
     case BRK_impl:
       printf("BRK_impl\n");
 
-      setFlag(p, I, 1);
+      setFlag(p, B, 1);
 
       pToAdd = 1;
-      cyclesToAdd = 7;
+      cToAdd = 7;
 
       break;
     case BEQ_rel:
       printf("BEQ_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & Z) == Z){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -115,11 +118,11 @@ int doCycle(struct processor* p){
       printf("BNE_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & Z) == 0){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -128,11 +131,11 @@ int doCycle(struct processor* p){
       printf("BMI_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & N) == N){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -141,11 +144,11 @@ int doCycle(struct processor* p){
       printf("BPL_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & N) == 0){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -154,11 +157,11 @@ int doCycle(struct processor* p){
       printf("BCS_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & C) == C){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -167,11 +170,11 @@ int doCycle(struct processor* p){
       printf("BCC_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & C) == 0){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -180,11 +183,11 @@ int doCycle(struct processor* p){
       printf("BVC_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & V) == 0){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -193,11 +196,11 @@ int doCycle(struct processor* p){
       printf("BVC_rel\n");
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       if((p->status & V) == V){
-        cyclesToAdd++;
-        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cyclesToAdd++;
+        cToAdd++;
+        if(p->pCount + (char)read_8 > 0xffff || p->pCount + (char)read_8 < 0x0000) cToAdd++;
         pToAdd += (char)read_8;
       }
 
@@ -208,7 +211,7 @@ int doCycle(struct processor* p){
       setFlag(p, C, 0);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case CLD_impl:
@@ -217,7 +220,7 @@ int doCycle(struct processor* p){
       setFlag(p, D, 0);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case CLI_impl:
@@ -226,7 +229,7 @@ int doCycle(struct processor* p){
       setFlag(p, I, 0);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case CLV_impl:
@@ -235,7 +238,7 @@ int doCycle(struct processor* p){
       setFlag(p, V, 0);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case DEX_impl:
@@ -247,7 +250,7 @@ int doCycle(struct processor* p){
       setFlag(p, N, (p->x & 0x80) == 0x80);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case DEY_impl:
@@ -259,7 +262,7 @@ int doCycle(struct processor* p){
       setFlag(p, N, (p->y & 0x80) == 0x80);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case INX_impl:
@@ -271,7 +274,7 @@ int doCycle(struct processor* p){
       setFlag(p, N, (p->x & 0x80) == 0x80);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case INY_impl:
@@ -283,7 +286,7 @@ int doCycle(struct processor* p){
       setFlag(p, N, (p->y & 0x80) == 0x80);
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case JMP_abs:
@@ -293,7 +296,7 @@ int doCycle(struct processor* p){
       p->pCount = read_16;
 
       pToAdd = 0;
-      cyclesToAdd = 5;
+      cToAdd = 3;
 
       break;
     case JMP_ind:
@@ -304,29 +307,31 @@ int doCycle(struct processor* p){
       indTo <<= 8;
       indTo |= *getVal(p, read_8, read_16, absN);
 
+      p->pCount = indTo;
+
       pToAdd = 0;
-      cyclesToAdd = 5;
+      cToAdd = 5;
 
       break;
     case JSR_abs:
       printf("JSR_abs\n");
 
-      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = p->pCount & 0xFF;
+      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = (p->pCount + 2) & 0xFF;
       p->stPtr++;
-      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = (p->pCount >> 8) & 0xFF;
+      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = ((p->pCount + 2) >> 8) & 0xFF;
       p->stPtr++;
 
       p->pCount = read_16;
 
       pToAdd = 0;
-      cyclesToAdd = 6;
+      cToAdd = 6;
 
       break;
     case NOP_impl:
       printf("NOP_impl\n");
 
       pToAdd = 1;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case PHA_impl:
@@ -336,16 +341,390 @@ int doCycle(struct processor* p){
       p->stPtr++;
 
       pToAdd = 1;
-      cyclesToAdd = 3;
+      cToAdd = 3;
 
       break;
+    case PHP_impl:
+      printf("PHP_impl\n");
+
+      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = p->status;
+      p->stPtr++;
+
+      pToAdd = 1;
+      cToAdd = 3;
+
+      break;
+    case PLA_impl:
+      printf("PLA_impl\n");
+
+      p->stPtr--;
+      p->a = *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr);
+      //*(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = 0x00;
+
+      setFlag(p, Z, p->a == 0);
+      setFlag(p, N, (p->a & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 4;
+
+      break;
+    case PLP_impl:
+      printf("PLP_impl\n");
+
+      p->stPtr--;
+      p->status = *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr);
+      //*(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = 0x00;
+
+      pToAdd = 1;
+      cToAdd = 4;
+
+      break;
+    case RTI_impl:
+      printf("RTI_impl\n");
+
+      p->stPtr--;
+      p->status = *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr);
+
+      p->stPtr--;
+      newPCount = *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) & 0xFF;
+      newPCount<<=8;
+
+      p->stPtr--;
+      newPCount |= (*(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) & 0xFF);
+
+      p->pCount = newPCount;
+
+      pToAdd = 1;
+      cToAdd = 6;
+
+      break;
+    case RTS_impl:
+      printf("RTS_impl\n");
+
+      reg_16 newPCount = 0;
+
+      p->stPtr--;
+      newPCount |= *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) & 0xFF;
+      newPCount<<=8;
+
+      p->stPtr--;
+      newPCount |= (*(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) & 0xFF);
+
+      p->pCount = ++newPCount;
+
+      pToAdd = 0;
+      cToAdd = 6;
+
+      break;
+    case SEC_impl:
+      printf("SEC_impl\n");
+
+      setFlag(p, C, 1);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case SED_impl:
+      printf("SED_impl\n");
+
+      setFlag(p, D, 1);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case SEI_impl:
+      printf("SEI_impl\n");
+
+      setFlag(p, I, 1);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TAX_impl:
+      printf("TAX_impl\n");
+
+      p->x = p->a;
+      setFlag(p, Z, p->x == 0);
+      setFlag(p, N, (p->x & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TAY_impl:
+      printf("TAY_impl\n");
+
+      p->y = p->a;
+      setFlag(p, Z, p->y == 0);
+      setFlag(p, N, (p->y & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TSX_impl:
+      printf("TSX_impl\n");
+
+      p->stPtr--;
+      p->x = *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr);
+
+      setFlag(p, Z, p->x == 0);
+      setFlag(p, N, (p->x & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TXA_impl:
+      printf("TXA_impl\n");
+
+      p->a = p->x;
+
+      setFlag(p, Z, p->a == 0);
+      setFlag(p, N, (p->a & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TYA_impl:
+      printf("TXA_impl\n");
+
+      p->a = p->y;
+
+      setFlag(p, Z, p->a == 0);
+      setFlag(p, N, (p->a & 0x80) == 0x80);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case TXS_impl:
+      printf("TXS_impl\n");
+
+      *(p->mem + SYSTEM_STACK_BEGIN + p->stPtr) = p->x;
+      p->stPtr++;
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+
+
+    //Non specific operations
+
+    case ADC_im:
+      printf("ADC_im\n");
+
+      adc(p, read_8, read_16, im);
+
+      pToAdd = 2;
+      cToAdd = 2;
+
+      break;
+    case ADC_zpg:
+      printf("ADC_zpg\n");
+
+      adc(p,read_8,read_16,zpg);
+
+      pToAdd = 2;
+      cToAdd = 3;
+
+      break;
+    case ADC_zpgX:
+      printf("ADC_zpgX\n");
+
+      adc(p,read_8,read_16,zpgX);
+
+      pToAdd = 2;
+      cToAdd = 4;
+
+      break;
+    case ADC_abs:
+      printf("ADC_abs\n");
+
+      adc(p,read_8,read_16,absN);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case ADC_absX:
+      printf("ADC_abs\n");
+
+      adc(p,read_8,read_16,absXP);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case ADC_absY:
+      printf("ADC_abs\n");
+
+      adc(p,read_8,read_16,absYP);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case ADC_indX:
+      printf("ADC_indX\n");
+
+      adc(p,read_8,read_16,indX);
+
+      pToAdd = 2;
+      cToAdd = 6;
+
+      break;
+    case ADC_indY:
+      printf("ADC_indY\n");
+
+      adc(p,read_8,read_16,indYP);
+
+      pToAdd = 2;
+      cToAdd = 5;
+
+      break;
+
+    case AND_im:
+      printf("AND_im\n");
+
+      and_(p,read_8,read_16,im);
+
+      pToAdd = 2;
+      cToAdd = 2;
+
+      break;
+    case AND_zpg:
+      printf("AND_zpg\n");
+
+      and_(p,read_8,read_16,zpg);
+
+      pToAdd = 2;
+      cToAdd = 3;
+
+      break;
+    case AND_zpgX:
+      printf("AND_zpgX\n");
+
+      and_(p,read_8,read_16,zpgX);
+
+      pToAdd = 2;
+      cToAdd = 4;
+
+      break;
+    case AND_abs:
+      printf("AND_abs\n");
+
+      and_(p,read_8,read_16,absN);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case AND_absX:
+      printf("AND_absX\n");
+
+      and_(p,read_8,read_16,absXP);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case AND_absY:
+      printf("AND_absY\n");
+
+      and_(p,read_8,read_16,absYP);
+
+      pToAdd = 3;
+      cToAdd = 4;
+
+      break;
+    case AND_indX:
+      printf("AND_indX\n");
+
+      and_(p,read_8,read_16,indX);
+
+      pToAdd = 2;
+      cToAdd = 6;
+
+      break;
+    case AND_indY:
+      printf("AND_indY\n");
+
+      and_(p,read_8,read_16,indY);
+
+      pToAdd = 2;
+      cToAdd = 4;
+
+      break;
+
+    case ASL_a:
+      printf("ASL_a\n");
+
+      asl(p, read_8, read_16, a);
+
+      pToAdd = 1;
+      cToAdd = 2;
+
+      break;
+    case ASL_zpg:
+      printf("ASL_zpg\n");
+
+      asl(p, read_8, read_16, zpg);
+
+      pToAdd = 2;
+      cToAdd = 5;
+
+      break;
+    case ASL_zpgX:
+      printf("ASL_zpgX\n");
+
+      asl(p, read_8, read_16, zpgX);
+
+      pToAdd = 2;
+      cToAdd = 6;
+
+      break;
+    case ASL_abs:
+      printf("ASL_abs\n");
+
+      asl(p, read_8, read_16, absN);
+
+      pToAdd = 3;
+      cToAdd = 6;
+
+      break;
+    case ASL_absX:
+      printf("ASL_absX\n");
+
+      asl(p, read_8, read_16, absX);
+
+      pToAdd = 3;
+      cToAdd = 7;
+
+      break;
+
+    case CMP_im:
+      printf("CMP_im\n");
+
+      cmp(p, read_8, read_16, im);
+
+      pToAdd = 2;
+      cToAdd = 2;
+
+      break;
+
     case LDA_im:
       printf("LDA_im\n");
 
       lda(p, read_8, read_16, im);
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
     case LDA_zpg:
@@ -354,16 +733,7 @@ int doCycle(struct processor* p){
       lda(p, read_8, read_16, zpg);
 
       pToAdd = 2;
-      cyclesToAdd = 3;
-
-      break;
-    case CMP_zpg:
-      printf("CMP_zpg");
-
-      cmp(p, read_8, read_16, zpg);
-
-      pToAdd = 2;
-      cyclesToAdd = 3;
+      cToAdd = 3;
 
       break;
     case LDX_im:
@@ -372,7 +742,7 @@ int doCycle(struct processor* p){
       ldx(p, read_8, read_16, im);
 
       pToAdd = 2;
-      cyclesToAdd = 2;
+      cToAdd = 2;
 
       break;
 
@@ -384,7 +754,7 @@ int doCycle(struct processor* p){
   }
 
   p->pCount += pToAdd;
-  p->cycles += cyclesToAdd;
+  p->cycles += cToAdd;
 
   printf("\n");
 
